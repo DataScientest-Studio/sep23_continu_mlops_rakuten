b7c29678918cbe2c1c6046de50aa081f1726b78d
alexl <alexluneau@yahoo.fr>
2024-01-18 00:00:32 +0100

    modif entrainement et airflow

---
+++ docker/airflow/dags/dag_entrainement.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 1, 17),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'dag_entrainement',
+    default_args=default_args,
+    description='DAG pour lancer l entrainement de notre model',
+    schedule_interval=timedelta(days=1),
+)
+
+docker_compose_up = BashOperator(
+    task_id='docker_compose_up',
+    bash_command='docker-compose -f /opt/docker/entrainement/docker-compose.yml up -d',
+    dag=dag,
+)
+
+docker_compose_down = BashOperator(
+    task_id='docker_compose_down',
+    bash_command='docker-compose -f /opt/docker/entrainement/docker-compose.yml down',
+    user='root',
+    dag=dag,
+)
+
+docker_compose_up >> docker_compose_down
