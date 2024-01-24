78930f5a9b2bfc0d0f057adb8272467e0a903b71
alexl <alexluneau@yahoo.fr>
2024-01-24 22:11:30 +0100

    clean du git dev, modification docker compose, readme

--- README.md
+++ README.md
@@ -1,60 +1,43 @@
-# ProjectTemplate
 
-## Explanations and Instructions
+## Context du projet
 
-This repository contains the files needed to initialize a project for your [DataScientest](https://datascientest.com/) training.
+Ce projet est issu d'un challenge Rakuten qui avait pour objectif la prédiction d'une classe de produits Rakuten à partir de sa description texte et image (27 catégories).
+Sa réalisation a eu lieu dans le cadre de la formation MLops de Datascientest (https://datascientest.com/).
+L'objectif de ce projet était de développer et de déployer en production l'algorithme de prédiction de catégorie en suivant une approche CI/CD (Intégration Continue/Déploiement Continu).
 
-It contains mainly the present README.md file and an application template [Streamlit](https://streamlit.io/).
+## Principe de fonctionnement de l'application
 
-**README**
+La mise en production de l'application est basée sur:
 
-The README.md file is a central element of any git repository. It allows you to present your project, its objectives, and to explain how to install and launch the project, or even how to contribute to it.
+    - 1 docker contenant une API générant les prédictions.
+    - 1 docker contenant le script de ré-entrainement et l'application MLFlow pour un suivi des ré-entrainements.
+    - 1 docker contenant airflow afin de vérifier le bon fonctionnement de l'API et de gérer le déclenchement des ré-entrainements.
+    - 1 docker rclone permettant le téléchargement des données images et textes depuis google drive.
 
-You will have to modify different sections of this README.md to include the necessary informations.
+## Déploiement
 
-- Complete the sections (`## Presentation and Installation` `## Streamlit App`) following the instructions in these sections.
-- Delete this section (`## Explanations and Instructions`)
+    1 - Cloner ce dépot Git
+    2 - Se déplacer dans le dossier docker  et générer un fichier .env :
+        Dans votre shell :
+        echo 'API_USERNAME = luffy' >.env
+        echo 'API_PASSWORD=<PWD de l API>' >>.env
+        echo -e "AIRFLOW_UID=$(id -u)" >>.env
+    3 -Dans le dossier docker, rentrer dans votre shell :
+        docker compose up
 
-**Streamlit Application**
+Le 1er lancement de "docker compose up" dure au moins 10 min (création des images docker), téléchargement des fichiers de google drive.
+Lors du 1er lancement, il se peut que l'image d'entrainement se lance avant la fin du téléchargement des fichiers de google drive, ce qui provoquera une erreur. Dans ce cas, attendre quelques minutes et refaire un "docker compose up".
 
-A [Streamlit] application template (https://streamlit.io/) is available in the [streamlit_app](streamlit_app) folder. You can use this template to start with your project.
+    4 - l'api est disponible et accessible localement à http://localhost:8080 . La documentation de l'api se trouve ici :  http://localhost:8080/docs
+    5 - MLflow est accessible ici : http://localhost:5000
+    6 - airflow est accessible ici : http://localhost:8080 :
+        3 DAGS disponibles :
+            - test_API : Test du bon fonctionnement de l'API. Tests d'authentification et de prédiction.
+            - training : Déclenche le ré-entrainement et l'incorporation de nouvelles données image et texte. 
 
-## Presentation and Installation
-
-Complete this section with a brief description of your project, the context (including a link to the DataScientest course), and the objectives.
-
-You can also add a brief presentation of the team members with links to your respective networks (GitHub and/or LinkedIn for example).
-
-**Example:**
-
-This repository contains the code for our project **PROJECT_NAME**, developed during our [Data Scientist training](https://datascientest.com/en/data-scientist-course) at [DataScientest](https://datascientest.com/).
-
-The goal of this project is to **...**
 
 This project was developed by the following team :
 
-- John Doe ([GitHub](https://github.com/) / [LinkedIn](http://linkedin.com/))
-- Martin Dupont ([GitHub](https://github.com/) / [LinkedIn](http://linkedin.com/))
-
-You can browse and run the [notebooks](./notebooks). 
-
-You will need to install the dependencies (in a dedicated environment) :
-
-```
-pip install -r requirements.txt
-```
-
-## Streamlit App
-
-**Add explanations on how to use the app.**
-
-To run the app (be careful with the paths of the files in the app):
-
-```shell
-conda create --name my-awesome-streamlit python=3.9
-conda activate my-awesome-streamlit
-pip install -r requirements.txt
-streamlit run app.py
-```
-
-The app should then be available at [localhost:8501](http://localhost:8501).
+- Alexandre Laroche ([GitHub](https://github.com/) / [LinkedIn](http://linkedin.com/))
+- Meryem Nedjwa Maameri ([GitHub](https://github.com/) / [LinkedIn](http://linkedin.com/))
+- Alexandre Luneau ([GitHub](https://github.com/alexluneau) / [LinkedIn](http://www.linkedin.com/in/alexandre-luneau-6b45a51))
--- api/api.py
+++ api/api.py
@@ -1,46 +1,44 @@
+from datetime import datetime, timedelta
+import re
+import os
+import shutil
 import pickle
+import jwt
 import numpy as np
 import pandas as pd
-import gdown
-import os.path
-from fastapi import FastAPI, HTTPException
 import tensorflow as tf
+from fastapi import FastAPI, Depends, HTTPException, status, File, UploadFile, Form
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from typing import Optional
 
-import urllib.request
-
+# NLTK et traitement de texte
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import RegexpTokenizer
+from unidecode import unidecode
+import nltk
 
 # --------
 # données
 # --------
 
 # données produits
-df = pd.read_csv('https://drive.google.com/uc?id=1g3QDdr55vGlCviXBzRTIiNpCSXxSUw-X')
+df = pd.read_csv('../docker/rclone/data/data/df_test.csv')
 df = df[["productid", "path", "Titre_annonce", "Description"]]
 
-# images
-google_drive_details = pd.read_csv('https://drive.google.com/uc?id=1gNsTA2Xb3CjLj193tACefqS_KMXNyBic')
-gdrive_images = google_drive_details[["id", "name"]]
 
-
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 # -------
 # modele
 # -------
-
-# google drive link : https://drive.google.com/file/d/1F8WjoOqhF2QLkceAQVFSJ2I9eI58bZJe/view?usp=drive_link
-file_id="1F8WjoOqhF2QLkceAQVFSJ2I9eI58bZJe"
-url = f'https://drive.google.com/uc?id={file_id}'
-output = '../../model.h5'
-
-if os.path.isfile(output):
-    print('model file already exist')
-else:
-    gdown.download(url, output, quiet=False)
-
-model = load_model(output)
-
+model = load_model('../data/bimodal.h5')
 
 with open("../data/label_encoder.pickle", "rb") as handle:
     le = pickle.load(handle)
@@ -53,11 +51,34 @@ def preprocess_image(image_path, resize=(200, 200)):
     im = tf.image.decode_png(im, channels=3)
     im = tf.image.resize(im, resize, method='nearest')
     return im
+    
+def preprocess_image_jpg(image_path, resize=(200, 200)):
+    im = tf.io.read_file(image_path)
+    im = tf.image.decode_jpeg(im, channels=3)
+    im = tf.image.resize(im, resize, method='nearest')
+    return im
+
+
+stop_words = set(stopwords.words('french'))
+stop_words.update(stopwords.words('english'))
+stop_words.update(stopwords.words('german'))
+
+def remove_accents(text):
+    return unidecode(text) if text else text
 
 def preprocess_text(text, tokenizer, max_len=100):
-    sequence = tokenizer.texts_to_sequences([text])
+    text = remove_accents(text)
+    text = text.lower()
+    text = re.sub('[0-9]', '', text)
+    text = re.sub('[^\w\s]', '', text)
+    nltk_tokenizer = RegexpTokenizer(r'\b[a-z]{2,}\b')
+    tokens = nltk_tokenizer.tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    lemmatized = [lemmatizer.lemmatize(token) for token in tokens]
+    cleaned = [word for word in lemmatized if word not in stop_words]
+    sequence = tokenizer.texts_to_sequences([' '.join(cleaned)])
     padded_sequence = pad_sequences(sequence, maxlen=max_len)
-    return padded_sequence[0]  # Retirer np.expand_dims dans la partie de prédiction
+    return padded_sequence[0]  
 
 # liste désignations des catégories
 categories = {
@@ -100,6 +121,92 @@ api = FastAPI(
     version="1.0")
 
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# Configuration pour le JWT
+SECRET_KEY = "secret"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRATION = 30
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+users_db = {
+
+    "luffy": {
+        "username": "luffy",
+        "name": "luffy",
+        "email": "luffy@chapeau.com",
+        "hashed_password": pwd_context.hash('123456789'),
+        "resource" : "rakuten",
+    },
+    
+}
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = users_db.get(username, None)
+    if user is None:
+        raise credentials_exception
+    return user
+
+@api.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    """
+    Description:
+    Cette route permet à un utilisateur de s'authentifier en fournissant un nom d'utilisateur et un mot de passe. Si l'authentification est réussie, elle renvoie un jeton d'accès JWT.
+
+    Args:
+    - form_data (OAuth2PasswordRequestForm, dépendance): Les données de formulaire contenant le nom d'utilisateur et le mot de passe.
+
+    Returns:
+    - Token: Un modèle de jeton d'accès JWT.
+
+    Raises:
+    - HTTPException(400, detail="Incorrect username or password"): Si l'authentification échoue en raison d'un nom d'utilisateur ou d'un mot de passe incorrect, une exception HTTP 400 Bad Request est levée.
+    """
+
+    user = users_db.get(form_data.username)
+    hashed_password = user.get("hashed_password")
+    if not user or not verify_password(form_data.password, hashed_password):
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRATION)
+    access_token = create_access_token(data={"sub": form_data.username}, expires_delta=access_token_expires)
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
 @api.get('/status', name="Status")
 async def get_status():
     """
@@ -109,6 +216,23 @@ async def get_status():
         'status': 1
     }
 
+@api.get("/secured")
+def read_private_data(current_user: str = Depends(get_current_user)):
+    """
+    Description:
+    Cette route renvoie un message uniquement si l'utilisateur est authentifié.
+
+    Args:
+    - current_user (str, dépendance): Le nom d'utilisateur de l'utilisateur actuellement authentifié.
+
+    Returns:
+    - JSON: Renvoie un JSON contenant un message de salutation sécurisé si l'utilisateur est authentifié, sinon une réponse non autorisée.
+
+    Raises:
+    - HTTPException(401, detail="Unauthorized"): Si l'utilisateur n'est pas authentifié, une exception HTTP 401 Unauthorized est levée.
+    """
+
+    return {"message": "go pour planter l api!"}
 
 @api.get('/predictcategory/{productid:int}', name="Prédiction")
 async def get_prediction(productid):
@@ -133,9 +257,7 @@ async def get_prediction(productid):
 
     if not df_product.empty:
 
-        image_file = str(df_product["path"].iloc[0]).split('/')[-1]
-        image_file_google_id = gdrive_images[gdrive_images['name']==image_file]
-        image_path, headers = urllib.request.urlretrieve("https://drive.google.com/uc?export=view&id=" + image_file_google_id['id'].iloc[0])
+        image_file = "../docker/rclone/data/images/image_test/" + str(df_product["path"].iloc[0]).split('/')[-1]
 
         texts_product = df_product.loc[:,("Titre_annonce", "Description")]
         if texts_product['Description'].iloc[0] == "nodata":
@@ -147,7 +269,7 @@ async def get_prediction(productid):
 
 
         # Prétraitement de l'image et du texte
-        image = preprocess_image(image_path)
+        image = preprocess_image(image_file)
         text = preprocess_text(text_input, tokenizer)
 
         # Ajustement de la forme des données pour correspondre à l'entrée du modèle
@@ -164,7 +286,7 @@ async def get_prediction(productid):
                 'product id': productid,
                 'product designation' : texts_product['Titre_annonce'].iloc[0],
                 'product description' : texts_product['Description'].iloc[0],
-                'product image' : "https://drive.google.com/uc?export=view&id=" + image_file_google_id['id'].iloc[0],
+                'product image' : str(df_product["path"].iloc[0]).split('/')[-1],
                 'predicted category': categories[predicted_label[0]]
                 }
         else:
@@ -172,3 +294,84 @@ async def get_prediction(productid):
     
     else:
         raise HTTPException(status_code=404, detail='Produit inconnu')
+
+
+
+@api.post("/get_prediction_input")
+async def get_prediction_input(titre: str = Form(...), 
+                               description: Optional[str] = Form(None), 
+                               image: UploadFile = File(...)):
+    """
+    Renvoie la prédiction de la catégorie du produit basée sur le titre, 
+    la description (optionnelle) et l'image fournis.
+    """
+
+    products_images_list = pd.read_csv('../docker/rclone/data/data/products_images_list.csv', header=0, index_col=0)
+    df_new_product = pd.read_csv('../docker/rclone/data/data/new_products.csv', header=0, index_col=0)
+
+    new_productid = max(products_images_list['productid']) + 1
+    new_imageid =  max(products_images_list['imageid']) + 1
+
+    # maj liste produits/images
+    products_images_list.loc[len(products_images_list)] = [new_productid, new_imageid]
+    products_images_list.to_csv('../docker/rclone/data/data/products_images_list.csv')
+
+    # Sauvegarde nouveau produit
+    df_new_product.loc[len(df_new_product)] = [titre, description, new_productid, new_imageid]
+    df_new_product.to_csv('../docker/rclone/data/data/new_products.csv')
+
+    # Sauvegarde de l'image
+    image_path = f"../docker/rclone/data/images/new_images/image_{new_imageid}_product_{new_productid}.jpg"
+    with open(image_path, "wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    # Prétraitement de l'image
+    image_processed = preprocess_image_jpg(image_path)
+    # tf.keras.utils.save_img('../docker/rclone/data/images/test.jpg', image_processed)
+
+    # Construction du texte pour le prétraitement
+    text_input = titre if description is None else f"{titre} {description}"
+    text_processed = preprocess_text(text_input, tokenizer)
+
+    # Ajustement de la forme des données pour correspondre à l'entrée du modèle
+    image_processed = np.expand_dims(image_processed, axis=0)  # Ajout d'une dimension pour le batch
+    text_processed = np.expand_dims(text_processed, axis=0)    # Ajout d'une dimension pour le batch
+
+    # Prédiction
+    prediction = model.predict([image_processed, text_processed])
+    predicted_class = np.argmax(prediction, axis=1)
+    predicted_label = le.inverse_transform(predicted_class)
+
+    if predicted_label[0] in categories:
+        return {
+            'predicted category': categories[predicted_label[0]]
+        }
+    else:
+        raise HTTPException(status_code=401, detail='Prediction de categorie invalide')
+
+ 
+class Item(BaseModel):
+    productID:str
+    categoryPredicted:str
+    categoryChangedByUser:str
+    categoryID:str
+
+@api.post('/Feedback')
+async def add_data(product_feedback : Item):
+    '''
+    Description :
+    -   Ajout du feedback utilisateur sur la performance du modèle à la base de données 
+    -   Pour l'intstant il ajoute seulement les données fournis par l'utilisateur dans le fichier ProductUSerFeedback.csv
+
+    Renvoie : Les différentes informations de l'article qui ont été ajouté à la base
+
+    '''
+    df_feedback = pd.read_csv('../docker/rclone/data/data/ProductUserFeedback.csv', header=0, index_col=0)
+
+    data_to_add = pd.DataFrame.from_dict(product_feedback).T
+    data_to_add['datetime'] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+    df_feedback.loc[len(df_feedback)] = list(data_to_add.loc[1])
+    df_feedback.to_csv('../docker/rclone/data/data/ProductUserFeedback.csv')
+
+    return product_feedback
