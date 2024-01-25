08acb35dd7eea653dd00d311ed1b7b1008bf30fa
Alex-Laroche <alexandre.laroche88@gmail.com>
2024-01-25 22:49:51 +0100

    maj training_dag

--- docker/airflow/dags/training_dag.py
+++ docker/airflow/dags/training_dag.py
@@ -2,7 +2,8 @@ from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-# from airflow.contrib.operators.docker_exec import DockerExecOperator
+
+import pandas as pd
 from mlflow.entities import ViewType
 import mlflow
 import os
@@ -17,13 +18,63 @@ training_dag = DAG(
         'start_date': days_ago(0),
     }
 )
- 
+
+
+# tâche pour vérifier la présence de nouvelles données
+def check_new_product( task_instance):
+   df_new_texts = pd.read_csv('/app/drive/data/new_products.csv', usecols=['designation', 'description', 'productid', 'imageid'])
+   if len(df_new_texts) == 0:
+      raise Exception("Pas de nouvelles données")
+
+check_new_product_script = PythonOperator(
+   task_id='check_new_product_script_task',
+   python_callable=check_new_product,
+   dag=training_dag
+)
+
+
+
+# tâches pour rajouter les textes et les images dans les données d'entrainement
+def get_new_texts():
+   df_x_train = pd.read_csv('/app/drive/data/X_train_update.csv', usecols=['designation', 'description', 'productid', 'imageid'])
+   df_new_texts = pd.read_csv('/app/drive/data/new_products.csv', usecols=['designation', 'description', 'productid', 'imageid'])
+   df_y_train = pd.read_csv('/app/drive/data/Y_train.csv', usecols=['prdtypecode'])
+   df_new_catagories = pd.read_csv('/app/drive/data/new_products_categories.csv', usecols=['prdtypecode'])
+
+   df_x_train_new = pd.concat([df_x_train, df_new_texts], axis=0, ignore_index=True)
+   df_x_train_new.to_csv('/app/drive/data/X_train_update.csv')
+   df_y_train_new = pd.concat([df_y_train, df_new_catagories], axis=0, ignore_index=True)
+   df_y_train_new.to_csv('/app/drive/data/Y_train.csv')
+
+   df_new_product = pd.DataFrame(columns=['designation', 'description', 'productid', 'imageid'])
+   df_new_product.to_csv('/app/drive/data/new_products.csv')
+   df_new_product_categories = pd.DataFrame(columns=['prdtypecode'])
+   df_new_product_categories.to_csv('/app/drive/data/new_products_categories.csv')
+
+add_new_texts_script = PythonOperator(
+   task_id='add_new_texts_script_task',
+   python_callable=get_new_texts,
+   dag=training_dag
+)
+
+
+add_new_images_script = BashOperator(
+   task_id="add_new_images_script_task",
+   bash_command="mv /app/drive/images/new_images/*.jpg /app/drive/images/image_train/",
+   dag=training_dag
+)
+
+
+# tâche pour lancer l'entrainement du modèle
 run_training_script = BashOperator(
    task_id="run_training_script_task",
    bash_command="docker exec docker-entrainement-1 python training_quick.py",
    dag=training_dag
 )
 
+
+
+# tâche pour récupérer le meilleur modèle
 def Get_best_model () : 
    mlflow.set_tracking_uri("http://entrainement:5000")
    #////////////////////////////////////////////////////////
@@ -68,5 +119,8 @@ accuracy_task = PythonOperator(
     python_callable=function_with_return
 )
 
-accuracy_task
 
+# dépendances
+check_new_product_script >> [add_new_texts_script, add_new_images_script]
+[add_new_texts_script, add_new_images_script] >> run_training_script
+run_training_script >> accuracy_task
